16448432480a058aaa5e0f99a4b7dd4632da6eea
msksaul <64514098+msksaul@users.noreply.github.com>
2020-12-08 15:46:11 -0600

    Optimization

--- primeNumber.js
+++ primeNumber.js
@@ -2,9 +2,10 @@ function isPrime(num) {
   //TODO
   let prime = true
   if(num <=1) return false
-  for(let i=2; i<num; i++){
+  for(let i=2; i<Math.sqrt(num); i++){
     if(num%i === 0 && num!==i){
       prime = false
+      break
     }
   }
   if(num%1 === 0 && num%num === 0 && prime) return true
